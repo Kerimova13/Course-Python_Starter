55092af922bff3e8d5ccf0be917e87b789c86fb1
Kerimova13 <49367339+Kerimova13@users.noreply.github.com>
2022-03-28 13:55:19 +0300

    Add files via upload

---
+++ Функції завдання_2.py
@@ -0,0 +1,12 @@
+# Верните количество букв в слове, которое передается в параметрах
+def count_letters(x):
+    return len(x)
+
+
+a= "room"
+v= count_letters(a)
+print(v)
+
+a1= "Mummy"
+v1= count_letters(a1)
+print(v1)
